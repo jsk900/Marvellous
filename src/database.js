3e07f001c56b50a824b5694fc86ef4dd9c908745
jsk900 <jsk900@yahoo.co.uk>
2018-01-11 10:56:53 +0100

    heroku

--- src/database.js
+++ src/database.js
@@ -8,9 +8,11 @@ let database;
 if (process.env.DATABASE_URL) {
     database = spicedPg(process.env.DATABASE_URL)
 
-} else {
-    database = spicedPg(`postgres:${secrets.user}:${secrets.password}@localhost:5432/marvel`)
 }
+
+// else {
+//     database = spicedPg(`postgres:${secrets.user}:${secrets.password}@localhost:5432/marvel`)
+// }
 
 // DB query function to insert new user data
 exports.insertUser = function(name, email, password) {
